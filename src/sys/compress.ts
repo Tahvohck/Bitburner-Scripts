3af0c9f3c1e2bb30c2c43117e53203617b18d77a
Tahvohck <Tahvohck@gmail.com>
2024-07-16 01:57:31 -0400

    Automatically transfer between time modes

--- src/sys/compress.ts
+++ src/sys/compress.ts
@@ -1,16 +1,4 @@
 import { NS } from "@ns";
-import { AutocompleteData, ScriptArg } from "@ns";
-
-const Options = {
-    revert: false
-}
-
-const FLAGS = [...Object.entries(Options)]
-
-export function autocomplete(data: AutocompleteData, args: ScriptArg[]) {
-    data.flags(FLAGS)
-    return [];
-}
 
 declare global {
     function nativeTimeout(h:TimerHandler, t: number | undefined, ...a:any[]): void
@@ -18,29 +6,20 @@ declare global {
 
 export async function main(ns:NS) {
     const nativeMatcher = /\[native code\]/
-    const options = ns.flags(FLAGS) as typeof Options
     const timeNormal = nativeMatcher.test(setTimeout.toString())
 
-    switch (options.revert) {
-        case false: {
-            if (timeNormal) {
-                ns.tprint("Compressing time!")
-                globalThis.nativeTimeout = setTimeout;
-                //@ts-expect-error Suppress whining about assigning to functions
-                setTimeout = overwrittenTimeout
-            } else {
-                ns.tprint("Time is already compressed")
-            }
+    switch (timeNormal) {
+        case true: {
+            ns.tprint("Compressing time!")
+            globalThis.nativeTimeout = setTimeout;
+            //@ts-expect-error Suppress whining about assigning to functions
+            setTimeout = overwrittenTimeout
             break;
         }
-        case true: {
-            if (!timeNormal) {
-                //@ts-expect-error Suppress whining about assigning to functions
-                setTimeout = globalThis.nativeTimeout;
-                ns.tprint("Time reverted.")
-            } else {
-                ns.tprint("Time is already normal")
-            }
+        case false: {
+            //@ts-expect-error Suppress whining about assigning to functions
+            setTimeout = globalThis.nativeTimeout;
+            ns.tprint("Time reverted.")
             break;
         }
     }
