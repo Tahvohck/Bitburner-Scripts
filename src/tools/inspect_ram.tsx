d4c0b0023e9f857f6c6ec44b92be9c6ca821c6f2
Tahvohck <Tahvohck@gmail.com>
2024-07-23 14:49:59 -0400

    Add utilization bar

--- src/tools/inspect_ram.tsx
+++ src/tools/inspect_ram.tsx
@@ -7,19 +7,52 @@ const Options = {
     all: false
 }
 let FLAGS = [...Object.entries(Options)]
+let infoLight: string;
+let infoText: string;
+let infoDark: string;
 
 export function autocomplete(data: AutocompleteData, args: ScriptArg[]) {
     data.flags(FLAGS)
     return [];
 }
 
+function Slider({value}: {value: number}) {
+    const sliderBoxStyle: React.CSSProperties = {
+        width: "20em",
+        display: "inline-flex",
+        backgroundColor: infoText,
+        position: "relative"
+    }
+    const sliderStyle: React.CSSProperties = {
+        flexGrow: `${value}`,
+        display: "inline-block"
+    }
+    const textStyle: React.CSSProperties = {
+        position: "absolute",
+        right: "1ch",
+        color: infoDark,
+    }
+    const valueText = value.toLocaleString(undefined, {maximumFractionDigits: 2, minimumFractionDigits: 2})
+    return <span style={sliderBoxStyle}>
+        <span style={{...sliderStyle, backgroundColor: infoLight}}>{"\u200B"}</span>
+        <span style={{...sliderStyle, flexGrow: `${100 - value}`}}>{"\u200B"}</span>
+        <span style={textStyle}>{valueText}%</span>
+    </span>
+}
+
 export async function main(ns:NS) {
     let options = ns.flags(FLAGS) as typeof Options
+    infoLight = ns.ui.getTheme().infolight
+    infoText = ns.ui.getTheme().info
+    infoDark = ns.ui.getTheme().infodark
 
     let sortedSources = RAM_SOURCES.sort((a, b) => b.max - a.max)
     let usage = sortedSources
         .filter((x) => x.used > 0 || options.all)
-        .map(x => <li>{x.toString()}</li>)
+        .map(x => {
+            const percentageUsed = x.used / x.max * 100
+            return <li>{x.toString()} <Slider value={percentageUsed}/></li>
+        })
     let unused = sortedSources
         .filter((x) => x.used == 0 && !options.all)
         .length
